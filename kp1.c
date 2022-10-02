a9f5591866cb3c49b45b41810a683dd3c8b14bf2
VitaBrrevis <vitalina.doronina@gmail.com>
2022-10-02 12:13:38 +0300

    god save me

--- kp1.c
+++ kp1.c
@@ -2,11 +2,11 @@
 #include <string.h>//strcmp in flag()
 #include <math.h>// all math
 
-#define N_SIDES 3 // const variable for n sides
-#define QUIT "yes\0"//global variable
+#define N_SIDES 3
+#define QUIT "yes\0"
 
 char sides[N_SIDES] = {'A', 'B', 'C'}; //names of the sides
-int indecies[N_SIDES * N_SIDES] = { 0, 1, 2, 1, 2, 0, 2, 0, 1} ; // indecies of indecies
+int indices[N_SIDES * N_SIDES] = { 0, 1, 2, 1, 2, 0, 2, 0, 1} ; // indices of indices
 /*
 i:0   0 1 2
 i:1   1 2 0
@@ -19,7 +19,7 @@ int check(double side1, double side2, double side3);//is used to stop or continu
 double area(double len[N_SIDES], int p); // finds the area of tg
 double height(double len[N_SIDES], int index, double root); //finds height
 double medians(double side1, double side2, double side3);// finds median
-double bisector(double , double , double, double p);//finds bisector
+double bisector(double side1, double side2, double side3, double p);//finds bisector
 
 int main() {
     do //cycle to loop the whole program
@@ -37,8 +37,8 @@ int main() {
                 test++;
                 break;
             }
-            if (!check(lengths[indecies[N_SIDES*num]],lengths[indecies[N_SIDES*num+1]], \
-            lengths[indecies[N_SIDES*num+2]])) { //checks the main equality of triangle
+            if (!check(lengths[indices[N_SIDES*num]],lengths[indices[N_SIDES*num+1]], \
+            lengths[indices[N_SIDES*num+2]])) { //checks the main equality of triangle
                 printf("[ERROR] sorry this triangle does not exist :c");
                 test++;
                 break;
@@ -51,12 +51,19 @@ int main() {
             printf("area equals %.2f cm\n", sqrt(root));
             printf("perimeter = %.2f cm\n", p);
             for (int i = 0; i < N_SIDES; i++) {
-                printf("to %c: height = %.2f cm; bisector = %.2f cm; median = %.2f cm\n", sides[i],
+                /*printf("to %c: height = %.2f cm; bisector = %.2f cm; median = %.2f cm\n", sides[i],
                        height(lengths, i, root), \
-                       bisector(lengths[indecies[N_SIDES*i]],lengths[indecies[N_SIDES*i+1]],\
-                       lengths[indecies[N_SIDES*i+2]],p / 2),//e.g. i=0: indecies[3*0+1]
-                       medians(lengths[indecies[N_SIDES*i]],lengths[indecies[N_SIDES*i+1]], \
-                       lengths[indecies[N_SIDES*i+2]]));
+                       bisector(lengths[indices[N_SIDES*i]],lengths[indices[N_SIDES*i+1]],\
+                       lengths[indices[N_SIDES*i+2]],p / 2),//e.g. i=0: indecies[3*0+1]
+                       medians(lengths[indices[N_SIDES*i]],lengths[indices[N_SIDES*i+1]], \
+                       lengths[indices[N_SIDES*i+2]]));*/
+                double side1 = lengths[indices[N_SIDES*i]];
+                double side2 = lengths[indices[N_SIDES*i+1]];
+                double side3 = lengths[indices[N_SIDES*i+2]];
+                printf("to %c: height = %.2f cm; bisector = %.2f cm; median = %.2f cm\n", sides[i],
+                      height(lengths, i, root), \
+                       bisector(side1,side2,side3 , p / 2),
+                      medians(side1,side2, side3));
             }
         }
     } while (flag()); //returns 1 if 'no' - I do not want to quit the program
